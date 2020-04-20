c3f1d98296bc93e60208866309161a2bca5d3ae9
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-04-19 21:45:32 -0500

    added all basic user-facing functionalities

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -39,26 +39,82 @@ function displayStore() {
                 result[i].stock_quantity);
         }
         console.log("----------------------------------------------------");;
+        buy();
     })
 
 }
 
-// function start() {
-//     inquirer
-//         .prompt([{
-//             name: "postOrBid",
-//             type: "list",
-//             message: "Would you like to [POST] an auction or [BID] on an auction?",
-//             choices: ["POST", "BID", "EXIT"]
-//         }])
-//         .then(function(answer) {
-//             // based on their answer, either call the bid or the post functions
-//             if (answer.postOrBid === "POST") {
-//                 postAuction();
-//             } else if (answer.postOrBid === "BID") {
-//                 bidAuction();
-//             } else {
-//                 connection.end();
-//             }
-//         });
-// }
+function buy(results) {
+    inquirer
+        .prompt([{
+                name: "idOfObject",
+                type: "input",
+                message: "Enter the ID of the object you want to purchase."
+            },
+            {
+                name: "number",
+                type: "input",
+                message: "How many would you like to buy?"
+            }
+        ])
+        .then(function(answers) {;
+
+            console.log("Checking storefront...");
+
+            connection.query("SELECT * FROM products", function(err, result) {
+                if (err) throw err;
+
+                var index = answers.idOfObject - 1;
+
+                var item = result[index].product_name;
+
+                var quantity = answers.number;
+
+                var stock = result[index].stock_quantity;
+
+                var price = result[index].price;
+
+                var total = quantity * price;
+
+                if (answers.idOfObject > 0 && answers.idOfObject < 11) {
+
+                    console.log("You selected " + item + " [quantity: " + quantity + "]");
+
+                    console.log("Checking inventory...");
+
+                    if (quantity <= stock) {
+                        console.log("Thank you for your order. Your total is $" + total + ".");
+                    } else {
+                        console.log("Sorry! We don't have the amount requested.");
+                    }
+
+                } else {
+                    console.log("Invalid item ID. Please try again.");
+                }
+
+
+            });
+
+        });
+}
+
+function orderAgain() {
+
+    inquirer
+        .prompt([{
+            type: 'list',
+            name: 'orderAgain',
+            message: 'Would you like to order again?',
+            choices: ['YES', 'NO'],
+        }])
+        .then(function(answer) {
+            if (answer === "YES") {
+                displayStore();
+            } else {
+                console.log("Thank you for using BAMazon!");
+                connection.end();
+            }
+
+        });
+
+}
